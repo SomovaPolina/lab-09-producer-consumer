9274e559f5a0b8a9e177031757ba053ca738ba50
polinka <zagigalka_60@mail.ru>
2021-04-20 12:31:22 +0300

    last

--- sources/source.cpp
+++ sources/source.cpp
@@ -1,18 +1,19 @@
 // Copyright 2020 Your Name <your_email>
 
-//#include "Downloader.hpp"
-//#include "Parametrs.hpp"
-//#include "Parser.hpp"
+#include "Downloader.hpp"
+#include "Parametrs.hpp"
+#include "Parser.hpp"
 
-//void Run_crauler(int argc, char* argv[]) {
-  //prepare_command_line(argc, argv);
-  //printParametrs();
-  //Downloader d(parametrs.downloader_threads);
-  //Parser p(parametrs.parser_threads);
-  //d.downloading(parametrs.url, parametrs.depth, p);
-//}
+int main(int argc, char* argv[]) {
+  prepare_command_line(argc, argv);
+  printParametrs();
+  Downloader d(parametrs.downloader_threads);
+  Parser p(parametrs.parser_threads);
+  d.downloading(parametrs.url, parametrs.depth, p);
+}
 
 /*
- ./cmake-build-debug/tests --url https://www.bbc.com/russian/news-54840425 \
- --depth 1 --network_threads 4 --parser_threads 4 --output\
+ ./cmake-build-debug/demo --url https://www.pierreherme.com/macarons \
+ --depth 2 --network_threads 4 --parser_threads 4 --output\
+ home/polinka/Рабочий стол/labs/lab-09-producer-consumer/output.txt
  */
